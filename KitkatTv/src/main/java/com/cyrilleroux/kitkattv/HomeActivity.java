e98173c4639a94acf393054314f35eebaec760cc
Cyril Leroux <cleroux@hubee.tv>
2014-11-20 18:12:11 +0100

    Updated styles

--- KitkatTv/src/main/java/com/cyrilleroux/kitkattv/HomeActivity.java
+++ KitkatTv/src/main/java/com/cyrilleroux/kitkattv/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.cyrilleroux.kitkattv;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,7 +9,7 @@ import android.widget.Button;
 /**
  * Created on 19/11/14.
  */
-public class MainActivity extends Activity implements View.OnClickListener {
+public class HomeActivity extends Activity implements View.OnClickListener {
 
     Button mButton1;
     Button mButton2;
@@ -17,11 +18,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        mButton1 = (Button) findViewById(android.R.id.button1);
-        mButton2 = (Button) findViewById(android.R.id.button2);
-        mButton3 = (Button) findViewById(android.R.id.button3);
+        mButton1 = (Button) findViewById(R.id.btn_browse_content);
+        mButton2 = (Button) findViewById(R.id.button2);
+        mButton3 = (Button) findViewById(R.id.button3);
 
         mButton1.setOnClickListener(this);
         mButton2.setOnClickListener(this);
@@ -32,13 +33,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case android.R.id.button1:
+
+            case R.id.btn_browse_content:
+                startActivity(new Intent(getApplicationContext(), ContentBrowserActivity.class));
                 break;
 
-            case android.R.id.button2:
+            case R.id.button2:
                 break;
 
-            case android.R.id.button3:
+            case R.id.button3:
                 break;
         }
     }
